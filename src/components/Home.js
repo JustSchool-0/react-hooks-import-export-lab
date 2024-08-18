de734a0c6d6e86c48e86374a0728842aa9784d65
Tori <7orivorian+github@gmail.com>
2024-08-18 17:56:14 -0400

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {city, username} from "../data/user";
 
 function Home() {
-  return (
-    <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
-    </div>
-  );
+    const h1Style = {
+        color: 'firebrick'
+    }
+    return (
+        <div id="home">
+            <h1 style={h1Style}>
+                {username} is a Web Developer from {city}
+            </h1>
+        </div>
+    );
 }
+
+export default Home;
